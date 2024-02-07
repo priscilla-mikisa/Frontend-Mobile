56a1f3771f7af8886e16a305e37165f7a105844d
priscilla mikisa <priscillamikisa@gmail.com>
2024-02-07 07:10:36 +0300

    added one kotlin file

---
+++ Main.kt
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var solarSystemPlanets:Byte = 8
+    solarSystemPlanets = 127
+
+    var numCountries: Short = 185
+    numCountries: Short = 32723
+
+    var weight: Float = 45.3F
+
+    var amount:Double = 32.64
+    var initial: Char ='L'
+
+    var school: String "Akirachix"
+
+    var classes = arrayOf("Lovelace","Adalab","Anitab")
+
+
+
+}
+
+
+
+
