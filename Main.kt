bfd19998bd32f66f3b0250fe30b9ad9a21e699fa
priscilla mikisa <priscillamikisa@gmail.com>
2024-02-07 07:57:40 +0300

    type1

--- Main.kt
+++ Main.kt
@@ -1,21 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    var solarSystemPlanets:Byte = 8
-    solarSystemPlanets = 127
-
-    var numCountries: Short = 185
-    numCountries: Short = 32723
-
-    var weight: Float = 45.3F
-
-    var amount:Double = 32.64
-    var initial: Char ='L'
-
-    var school: String "Akirachix"
-
-    var classes = arrayOf("Lovelace","Adalab","Anitab")
-
+var studentName = "Mikisa Priscilla"
+    println("name")
+    var age = 20
+    println("age")
+    var phoneNum = "0741817334"
+    println("phoneNum")
+    var weightInKg = 49
+    println("weight")
+    val kenyanCitizen = "true"
+    println(kenyanCitizen)
 
 
 }
